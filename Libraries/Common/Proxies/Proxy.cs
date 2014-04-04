5010b49b8b6c040ea5b9bbb97e939ff636c99d5b
Martin Kraner <martinkraner@outlook.com>
2014-04-04 13:52:57 +0200

    Added missing Proxy<T> base class from previous commit.

---
+++ Libraries/Common/Proxies/Proxy.cs
@@ -0,0 +1,24 @@
+﻿namespace Frost.Common.Proxies {
+
+    /// <summary>An abstract base class used for proxing classes</summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class Proxy<T> where T : class {
+        protected readonly T Entity;
+
+        /// <summary>Initializes a new instance of the <see cref="Proxy{T}"/> class.</summary>
+        /// <param name="entity">The entity to proxy.</param>
+        protected Proxy(T entity) {
+            Entity = entity;
+        }
+
+        /// <summary>Gets the proxied entity.</summary>
+        /// <value>The proxied entity.</value>
+        public T ProxiedEntity { get { return Entity; } }
+
+        /// <summary>Returns a string that represents the current object.</summary>
+        /// <returns>A string that represents the current object.</returns>
+        public override string ToString() {
+            return Entity.ToString();
+        }
+    }
+}
