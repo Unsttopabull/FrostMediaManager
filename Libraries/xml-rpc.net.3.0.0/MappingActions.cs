2c8152ba5286b2f343110b26a16abc46632ddec8
Martin Kraner <martinkraner@outlook.com>
2014-05-19 15:36:31 +0200

    Removing XmlRpc.NET, XamlControls, PHPtoNet, SharpMediaInfo, SharpLanguageDetect, GettextMarkupExtension to be added as a sub-module.

--- Libraries/xml-rpc.net.3.0.0/MappingActions.cs
+++
@@ -1,8 +0,0 @@
-namespace CookComputing.XmlRpc
-{
-    public class MappingActions
-    {
-        public NullMappingAction NullMappingAction { get; set; }
-        public EnumMapping EnumMapping { get; set; }
-    }
-}
